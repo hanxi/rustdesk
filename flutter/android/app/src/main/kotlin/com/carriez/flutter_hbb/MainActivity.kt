a78ef97c09e753bb72b5bc9dd12314d45e04d7e1
涵曦 <im.hanxi@gmail.com>
2024-04-12 10:24:03 +0800

    fix build error

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainActivity.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainActivity.kt
@@ -12,6 +12,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Build
+import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
 import android.view.WindowManager
